73c26b0a87396472eb6cc94d210853fc128c5cc3
CHR <69011494+CHR-design@users.noreply.github.com>
2023-03-05 03:17:13 +0800

    Update app.py
    修改文件

--- app.py
+++ app.py
@@ -1,44 +1,37 @@
-# 引入所需的库
+import openai
 import streamlit as st
-import requests
-import json
 
-# 定义API密钥和API URL
-OPENAI_API_KEY = "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"
-OPENAI_API_URL = "https://api.openai.com/v1/engines/davinci-codex/completions"
+# 设置OpenAI API密钥
+openai.api_key = "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"
 
-# 定义Streamlit应用程序
+# 定义一个函数，使用OpenAI GPT-3生成代码
+def generate_code(prompt, language):
+    model_engine = "text-davinci-002"
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=2048,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    message = completions.choices[0].text
+    return message
+
+# Streamlit 应用程序主体
 def app():
-    # 设置应用程序标题
-    st.title("Code Generation")
+    st.title("OpenAI GPT-3 代码生成器")
+    st.write("请输入您的需求内容和编程语言，让GPT-3帮助您生成代码！")
 
-    # 添加用户输入框
-    language = st.text_input("Programming Language", "")
-    content = st.text_input("Content", "")
+    # 获取用户输入
+    prompt = st.text_input("输入您的需求内容")
+    language = st.text_input("输入编程语言")
 
-    # 添加“生成代码”按钮
-    if st.button("Generate Code"):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {OPENAI_API_KEY}",
-        }
-        data = {
-            "prompt": f"Generate code in {language} for \"{content}\"",
-            "temperature": 0.7,
-            "max_tokens": 2048,
-            "n": 1,
-            "stop": "###",
-        }
-        response = requests.post(OPENAI_API_URL, headers=headers, data=json.dumps(data))
-        if response.status_code == 200:
-            response_data = json.loads(response.text)
-            choices = response_data["choices"][0]["text"]
-            code = choices[: choices.index("\n")]
-            st.write(code)
-        else:
-            st.write("Failed to generate code. Please try again later.")
+    # 按下按钮以生成代码
+    if st.button("生成代码"):
+        # 调用 generate_code() 函数
+        code = generate_code(prompt, language)
+        st.code(code, language=language)
 
-
-# 运行Streamlit应用程序
-if __name__ == "__main__":
+if __name__ == '__main__':
     app()
