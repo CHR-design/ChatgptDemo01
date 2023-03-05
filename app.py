0e7af577b13b44aebbfd80b6ec600be09c8509e0
CHR <69011494+CHR-design@users.noreply.github.com>
2023-03-05 13:24:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,19 +21,21 @@ def generate_code(language, prompt):
     code = message.strip()
     return code
 
-# 设置Streamlit应用程序的外观和布局
-st.set_page_config(page_title="Generate Code", page_icon=":computer:", layout="wide")
+def app():
+    st.title("OpenAI GPT-3 代码生成器")
+    st.write("请输入您的需求内容和编程语言，让GPT-3帮助您生成代码！")
 
-st.title("OpenAI GPT-3 代码生成器")
-st.write("请输入您的需求内容和编程语言，让GPT-3帮助您生成代码！")
+    # 获取用户输入
+    prompt = st.text_input("输入您的需求内容")
+    language = st.selectbox("编程语言", ["Python", "JavaScript", "Java"])
 
-# 允许用户选择编程语言和需求
-language = st.selectbox("编程语言", ["Python", "JavaScript", "Java"])
-prompt = st.text_input("输入您的需求内容", "generate Fibonacci sequence")
+    # 按下按钮以生成代码
+    if st.button("生成代码"):
+        # 调用 generate_code() 函数
+        code = generate_code(language,prompt)
+        st.code(code, language=language.lower())
 
-# 当用户单击按钮时，使用GPT-3生成代码
-if st.button("Generate Code"):
-    code = generate_code(language, prompt)
-    # 显示生成的代码
-    st.code(code, language=language.lower())
+if __name__ == '__main__':
+    app()
+    
 
