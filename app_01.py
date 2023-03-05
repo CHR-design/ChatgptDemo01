c2e6b02f1a24a32282a9eef58ac2908ac46cb2d2
CHR <69011494+CHR-design@users.noreply.github.com>
2023-03-06 01:14:22 +0800

    Update app_01.py

--- app_01.py
+++ app_01.py
@@ -3,28 +3,34 @@ import openai
 import markdown
 
 # Set OpenAI API key
-openai.api_key = st.secrets["openai"]["api_key"]
+openai.api_key = "st.secrets["openai"]["api_key"]"
 
-# Define app
-def app():
-    # Initialize session state
-    if "logged_in" not in st.session_state:
-        st.session_state["logged_in"] = False
 
-    session_state = st.session_state
+# Initialize session state
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
 
-    # Define homepage layout
-    def home_page():
+session_state = st.session_state
+
+
+# Define homepage layout
+def home_page():
+
         st.title("解惑学院")
         st.write("欢迎来到解惑学院！")
 
         # Add navigation options
         st.write("导航：")
         if st.button("个人简历"):
-            if not session_state.logged_in:
+            # Check if user is logged in
+            if not session_state["logged_in"]:
+                st.warning("请先登录")
                 st.experimental_set_query_params(page="login")
+                return
+                # stop rendering here
             else:
                 st.experimental_set_query_params(page="resume")
+                return
 
         if st.button("代码编写"):
             st.experimental_set_query_params(page="code")
@@ -35,35 +41,26 @@ def app():
         if st.button("看医生"):
             st.experimental_set_query_params(page="doctor")
 
-    def login_page():
+
+def login_page():
         st.title("Login")
         # Add login form
         with st.form(key="login_form"):
             username = st.text_input("用户名")
             password = st.text_input("密码", type="password")
-            submit = st.form_submit_button("Login")
+            submit = st.form_submit_button("登录")
 
             # Verify user credentials and set session state
             if submit:
+                st.warning(session_state["logged_in"])
+                # return
                 if username == "admin" and password == "admin":
-                    session_state.logged_in = True
+                    session_state["logged_in"] = True
                     st.experimental_set_query_params(page="home")
 
-    # not logged in
-    if st.experimental_get_query_params().get("page") == "login":
-        login_page()
-    elif st.experimental_get_query_params().get("page") == "resume":
-        resume_page()
-    else:
-        home_page()
 
-    # Update page if user logs in
-    if session_state.logged_in:
-        st.experimental_set_query_params(page="home")
-        home_page()
 
-    # Define resume page layout
-    def resume_page():
+def resume_page():
         st.title("个人简历")
         st.write("在这里输入您的个人信息、教育经历、工作经历、个人优势/技能等信息，并点击 '生成简历'。")
 
@@ -76,6 +73,8 @@ def app():
             submit = st.form_submit_button("生成简历")
 
             # Generate resume using OpenAI API and show it as Markdown
+
+
             if submit:
                 prompt = f"Generate a resume based on the following information:\n\n{personal_info}\n\n{education}\n\n{work_experience}\n\n{skills}\n\n"
                 response = openai.Completion.create(
@@ -90,5 +89,43 @@ def app():
                 st.write(markdown.markdown(resume))
                 st.markdown(f"[Download Markdown](data:text/plain,{resume})", unsafe_allow_html=True)
 
+# def code_page():
+# def bug_page():
+# def homework_page():
+# def doctor_page():
+
+# Define app
+def app():
+
+    page = st.experimental_get_query_params().get("page")
+    # not logged in
+    if not session_state["logged_in"] and page != "login":
+        st.experimental_set_query_params(page="login")
+        # login_page()
+    elif session_state["logged_in"] and page == "resume":
+        st.experimental_set_query_params(page="home")
+        # resume_page()
+    else:
+        home_page()
+
+    if page == "login":
+        login_page()
+    elif page == "resume":
+        resume_page()
+    # elif page == "code":
+    #     code_page()
+    # elif page == "bug":
+    #     bug_page()
+    # elif page == "homework":
+    #     homework_page()
+    # elif page == "doctor":
+    #     doctor_page()
+    else:
+        home_page()
+
+
+
 if __name__ == '__main__':
+
     app()
+
