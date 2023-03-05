fd89e29a55a28de1b6f731979dda7d6add54a7e4
CHR <69011494+CHR-design@users.noreply.github.com>
2023-03-05 15:50:27 +0800

    Update app_01.py

--- app_01.py
+++ app_01.py
@@ -4,35 +4,31 @@ import openai
 # Set OpenAI API key
 openai.api_key = st.secrets["openai"]["api_key"]
 
-
-
-
-
-
-
-
 # Define app
 def app_01():
     # Initialize session state
-    session_state = st.session_state.get(logged_in=False)
+    if "logged_in" not in st.session_state:
+        st.session_state["logged_in"] = False
+
+    session_state = st.session_state
 
     # Define homepage layout
-def home_page():
-    st.title("解惑学院")
-    st.write("欢迎来到解惑学院！")
+    def home_page():
+        st.title("解惑学院")
+        st.write("欢迎来到解惑学院！")
 
-    # Add navigation options
-    st.write("导航：")
-    if st.button("个人简历"):
-        st.experimental_set_query_params(page="resume")
-    if st.button("代码编写"):
-        st.experimental_set_query_params(page="code")
-    if st.button("检查代码Bug"):
-        st.experimental_set_query_params(page="bug")
-    if st.button("作业解答"):
-        st.experimental_set_query_params(page="homework")
-    if st.button("看医生"):
-        st.experimental_set_query_params(page="doctor")
+        # Add navigation options
+        st.write("导航：")
+        if st.button("个人简历"):
+            st.experimental_set_query_params(page="resume")
+        if st.button("代码编写"):
+            st.experimental_set_query_params(page="code")
+        if st.button("检查代码Bug"):
+            st.experimental_set_query_params(page="bug")
+        if st.button("作业解答"):
+            st.experimental_set_query_params(page="homework")
+        if st.button("看医生"):
+            st.experimental_set_query_params(page="doctor")
         
     # Define login page layout
     def login_page():
