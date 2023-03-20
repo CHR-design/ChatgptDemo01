7f4cf1e8e5070aff728e6b79d7a1b6a5926d961a
CHR <69011494+CHR-design@users.noreply.github.com>
2023-03-21 00:25:53 +0800

    Update zhouyi.py

--- zhouyi.py
+++ zhouyi.py
@@ -9,13 +9,7 @@ openai.api_key = st.secrets["openai"]["api_key"]
 
 # 定义一个函数，使用GPT-3生成代码
 
-def generate_code(prompt, language):
 
-    model_engine = "text-davinci-003"
-
-    completions = openai.Completion.create(
-
-        engine=model_engine,
 
 
     
