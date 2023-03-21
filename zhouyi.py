8dd269a5a2d2b5c24a282d8a548deeb499898f0f
CHR <69011494+CHR-design@users.noreply.github.com>
2023-03-21 09:18:15 +0800

    Update zhouyi.py

--- zhouyi.py
+++ zhouyi.py
@@ -7,13 +7,6 @@ import streamlit as st
 
 openai.api_key = st.secrets["openai"]["api_key"]
 
-# 定义一个函数，使用GPT-3生成代码
-
-
-
-
-    
-
 
 # 创建 Streamlit 应用程序
 
@@ -25,13 +18,11 @@ def main():
 
     name = st.text_input("请输入您的姓名")
 
-    birth_date = st.text_input("请输入您的出生日期（格式为 YYYY-MM-DD）")
-
-    birth_time = st.text_input("请输入您的出生时间（格式为 HH:MM）")
+    birth_date = st.text_input("请输入您的农历出生日期（格式为 YYYY年MM月DD日）")
 
     location = st.text_input("请输入您的居住地")
 
-    question = st.text_input("请输入您的问题")
+    question = st.text_input("请输入您的问题所在范围(如，婚姻、做生意等)")
 
     # 添加“提交”按钮
 
@@ -43,7 +34,7 @@ def main():
 
             engine="davinci",
 
-            prompt=f"姓名：{name}\n出生日期：{birth_date}\n出生时间：{birth_time}\n居住地：{location}\n问题：{question}",
+            prompt=f"假设你是一名占卜师，熟读周易，现在有人向你求{question}，他的姓名叫：{name}\n农历{birth_date}出生\n出生\n住在{location}\n",
 
             temperature=0.7,
 
